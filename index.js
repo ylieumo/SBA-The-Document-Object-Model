c748fe80e124dc09faf6ead6ee762f38e61ed51a
ylieumo <yoldjampou@gmail.com>
2023-12-14 18:21:30 -0500

    more informations

--- index.js
+++ index.js
@@ -3,22 +3,45 @@ document.write(Date());
 
 const welcomecontainer = document.getElementById('welcomeContainer');
     const greeting = welcomecontainer.firstChild;
-   const featureList = greeting.nextSibling;
    const firstFeature = featureList.firstChild;
    const lastFeature = featureList.lastChild;
-  const nextFeature = firstFeature.nextElementSibling;
-
-
-
-
-
-
-
-  let tittleEl = document.getElementById("Welcome");
+ 
+ let tittleEl = document.getElementById("Welcome");
   tittleEl.style.textAlign = "center"
 
+  document.getElementById("container").style.color = "green";
 
-  function validateForm() {
+
+  function myFunction() {
+    const myNodelist = document.querySelectorAll("featureItem");
+    for (let i = 0; i < myNodelist.length; i++) {
+      myNodelist[i].style.color = "red";
+    }
+  }
+
+  var features = document.querySelectorAll('.featureItem');
+  features.forEach(function(feature) {
+    document.getElementById("demo").innerHTML = NewPage();
+  });
+
+
+ a.addEventListener("click", myFunction);
+
+function myFunction() {
+  alert ("welcome!");
+}
+function validatePassword() {
+    const passwordInput = document.getElementById('password');
+ const errorSpan = document.getElementById('passwordError');
+
+    if (passwordInput.value.length < 8) {
+      errorSpan.textContent = 'Password must be at least 8 characters long.';
+    } else {
+      errorSpan.textContent = '';
+    }
+  }
+  
+  function validatePassword() {
     let x = document.forms["myForm"]["fname"].value;
     if (x == "") {
       alert("Name must be filled out");
